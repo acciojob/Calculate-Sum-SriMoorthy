8d075698e96fdd7e5f26fc7743f46d8144324ce2
SriMoorthy <srimoorthy27@gmail.com>
2024-10-07 20:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let num1 = document.getElementById('n1')
+let num2 = document.getElementById('n2')
+let sm = document.getElementById('sum')
+let sum = 0;
+
+function Calculate() {
+	
+	const n1 = parseFloat(num1.value)
+	const n2 = parseFloat(num2.value)
+	
+	sum = n1+n2
+	sm.innerHTML = sum
+}
+
